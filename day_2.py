ac32c2ffcbae0fcba92d730679c238bec0a090a9
J.W.Maas <wietskemaas@gmail.com>
2021-12-07 14:26:14 +0100

    day 2 Q2

--- day_2.py
+++ day_2.py
@@ -1,6 +1,5 @@
 # solving the questions of day 2 of the adevent of code
 # Load the input of day 2
-input2 = open('day_2.txt').read().splitlines()
 
 dictionary = dict()
 with open('day_2.txt', 'r') as input2:
@@ -19,4 +18,26 @@ up = sum(dictionary['up'])
 dept = down - up
 
 answer = forward*dept
-print(answer)
+
+# question 2
+command_list = []
+
+with open('day_2.txt', 'r') as input2:
+    for line in input2:
+        command,value = line.split()
+        command_list.append([command, int(value)])
+
+hor = 0
+aim = 0
+dept2 = 0
+for entry in command_list:
+    if entry[0] == 'forward':
+        hor = hor + entry[1]
+        dept2 = dept2 + aim*entry[1]
+    elif entry[0] == 'up':
+        aim = aim - entry[1]
+    elif entry[0] == 'down':
+        aim = aim + entry[1]
+
+answer2 = hor*dept2
+print(answer2)
